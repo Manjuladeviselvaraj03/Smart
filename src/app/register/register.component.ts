0de4a0a758173fafd4c3cd524ef26416aa5246fa
Manjuladeviselvaraj03 <manjuladeviselvaraj12@gmai.com>
2025-11-06 21:29:55 +0530

    local changes done

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+   register!:FormGroup;
+
+ 
+   constructor (private fb:FormBuilder ){}
+ ngOnInit(){
+    this. register=this.fb.group({
+    name:['',Validators.required,Validators.pattern('[A-Za-z. ]+$')],
+    email:['',Validators.required,Validators.pattern('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/')],
+    password:['',Validators.required,Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')],
+    phoneno:['',Validators.required,Validators.pattern('^[0-9]{10}$')]});
+ 
+  
+}
+ get f(){
+   return this.register.controls;
+ }
 
 }
+
+
