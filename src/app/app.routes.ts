0ddf06337e96d68d0c2e4b2e9a90892b863517ab
Manjuladeviselvaraj03 <manjuladeviselvaraj12@gmai.com>
2025-11-10 11:10:07 +0530

    frontlayout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { FrontlayoutComponent } from './frontlayout/frontlayout.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
 
     {path:'register',component: RegisterComponent},
     {path:'login',component:LoginComponent},
     { path: '', component: FrontlayoutComponent },
-    {path:'dashboard',component:DashboardComponent}
+    {path:'dashboard',component:DashboardComponent},
+    {path:'about',component:AboutComponent},
+    {path:'contact',component:ContactComponent}
  
 ];
