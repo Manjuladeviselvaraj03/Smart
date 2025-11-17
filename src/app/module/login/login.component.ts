9ac75a2741dd10af4a578b97b730cf38c3cf9443
Dharshana2204 <dharshanak2204@gmail.com>
2025-11-17 18:26:00 +0530

    Changes done

--- src/app/module/login/login.component.ts
+++ src/app/module/login/login.component.ts
@@ -6,7 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { DataserviceService } from '../../sharedResource/dataservice.service';
 
 import { ToastModule } from 'primeng/toast';
@@ -23,6 +23,7 @@ import { InputTextModule } from 'primeng/inputtext';
     ToastModule,
     ButtonModule,
     InputTextModule,
+    RouterLink
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
